e5cebf0c2fb8dfe7c7763fb85b39c4258d858675
Joanne Santhosh <99355156+joannesanthosh@users.noreply.github.com>
2022-12-01 22:05:40 -0500

    update

--- constants.py
+++ constants.py
@@ -4,7 +4,7 @@
 # Created on: Nov 2022
 # This is the constants file
 
-# Pybadge screen size is 160x128 and sprites are 16x16
+# PyBadge screen size is 160x128 and sprites are 16x16
 SCREEN_X = 160
 SCREEN_Y = 128
 SCREEN_GRID_X = 10
@@ -25,12 +25,20 @@ SPRITE_MOVEMENT_SPEED = 1
 # Using for button state
 button_state = {
     "button_up": "up",
-    "button_just_pressed": "just_pressed",
+    "button_just_pressed": "just pressed",
     "button_still_pressed": "still pressed",
     "button_released": "released",
 }
-# new pallet for filled text
+# new pallet for red filled text
+WHITE_BLANK_PALETTE = (
+    b"\xf8\x1f\x00\x00\xcey\xff\xff\xf8\x1f\x00\x19\xfc\xe0\xfd\xe0"
+    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+)
 RED_PALETTE = (
+    b"\xff\xff\x00\x00\xcey\x66\xff\xff\xff\xff\xff\xff\xff\xfd\xe0"
+    b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00"
+)
+BLUE_PALETTE = (
     b"\xff\xff\x00\x22\xcey\x22\xff\xff\xff\xff\xff\xff\xff\xff\xff"
     b"\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff"
 )
